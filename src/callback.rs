8ae1da3a25dc887275defa54b45854f2ca625718
Rodolfo Araujo <rodoufu@gmail.com>
2020-02-27 22:22:56 -0300

    CallbackRegistry.

--- src/callback.rs
+++ src/callback.rs
@@ -1,21 +1,32 @@
 use std::rc::Rc;
 
-
-///
-/// CallbackManager tracks callbacks and then call all then when requested.
-///
 #[derive(Default)]
-pub struct CallbackManager<ParamType: Copy> {
-	/// It is using a Box for the futures here cause the vector needs a sized type.
-	callbacks: Vec<(Box<dyn Fn(ParamType)>, Rc<CallbackHandler>)>,
+pub struct CallbackManager {
+	manager: CallbackRegistry<()>,
 }
 
-#[derive(Debug, Clone, Hash, PartialEq, Eq, Default)]
-pub struct CallbackHandler {}
+impl CallbackManager {
+	pub fn add(&mut self, callback: Box<dyn Fn()>) -> Rc<()> {
+		self.manager.add(Box::new(move |_| callback()))
+	}
 
-impl<ParamType: Copy> CallbackManager<ParamType> {
-	pub fn add(&mut self, callback: Box<dyn Fn(ParamType)>) -> Rc<CallbackHandler> {
-		let resp = Rc::new(CallbackHandler {});
+	pub fn run_all(&mut self) {
+		self.manager.run_all(())
+	}
+}
+
+///
+/// CallbackRegistry tracks callbacks and then call all then when requested.
+///
+#[derive(Default)]
+pub struct CallbackRegistry<ParamType: Copy> {
+	/// It is using a Box for the futures here cause the vector needs a sized type.
+	callbacks: Vec<(Box<dyn Fn(ParamType)>, Rc<()>)>,
+}
+
+impl<ParamType: Copy> CallbackRegistry<ParamType> {
+	pub fn add(&mut self, callback: Box<dyn Fn(ParamType)>) -> Rc<()> {
+		let resp = Rc::new(());
 		self.callbacks.push((callback, resp.clone()));
 		resp
 	}
@@ -31,18 +42,52 @@ impl<ParamType: Copy> CallbackManager<ParamType> {
 
 #[cfg(test)]
 mod tests {
-	use crate::callback::CallbackManager;
+	use crate::callback::{
+		CallbackManager,
+		CallbackRegistry,
+	};
 
 	#[test]
 	fn no_param_callback() {
-		let mut my_callback: CallbackManager<()> = Default::default();
+		let mut my_callback: CallbackManager = Default::default();
+		println!("Out of callback no");
+
+		let _first_handler = my_callback.add(Box::new(||
+			println!("First callback no param")
+		));
+		let _second_handler = my_callback.add(Box::new(||
+			println!("Second callback no param")
+		));
+		my_callback.add(Box::new(|| {
+			println!("First dropped callback no param");
+			assert!(false);
+		}));
+		{
+			let _second_droped_handler = my_callback.add(Box::new(|| {
+				println!("Second dropped callback no param");
+				assert!(false);
+			}));
+		}
+
+		my_callback.run_all();
+	}
+
+	#[test]
+	fn empty_param_callback() {
+		let mut my_callback: CallbackRegistry<()> = Default::default();
 		println!("Out of callback");
 
 		let _first_handler = my_callback.add(Box::new(|_| println!("First callback")));
 		let _second_handler = my_callback.add(Box::new(|_| println!("Second callback")));
-		my_callback.add(Box::new(|_| println!("First dropped callback")));
+		my_callback.add(Box::new(|_| {
+			println!("First dropped callback");
+			assert!(false);
+		}));
 		{
-			my_callback.add(Box::new(|_| println!("Second dropped callback")));
+			let _second_droped_handler = my_callback.add(Box::new(|_| {
+				println!("Second dropped callback");
+				assert!(false);
+			}));
 		}
 
 		my_callback.run_all(());
@@ -50,7 +95,7 @@ mod tests {
 
 	#[test]
 	fn u32_param_callback() {
-		let mut my_callback: CallbackManager<u32> = Default::default();
+		let mut my_callback: CallbackRegistry<u32> = Default::default();
 		println!("Out of callback u32");
 
 		let _first_handler = my_callback.add(Box::new(|x|
@@ -59,9 +104,15 @@ mod tests {
 		let _second_handler = my_callback.add(Box::new(|x|
 			println!("Second callback: {}", x)
 		));
-		my_callback.add(Box::new(|x| println!("First dropped callback: {}", x)));
+		my_callback.add(Box::new(|x| {
+			println!("First dropped callback: {}", x);
+			assert!(false);
+		}));
 		{
-			my_callback.add(Box::new(|x| println!("Second dropped callback: {}", x)));
+			let _second_droped_handler = my_callback.add(Box::new(|x| {
+				println!("Second dropped callback: {}", x);
+				assert!(false);
+			}));
 		}
 
 		my_callback.run_all(7);
