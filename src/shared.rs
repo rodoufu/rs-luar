8edf8dc262ea05abc5e7598b011a5548647ec57f
Rodolfo Araujo <rodoufu@gmail.com>
2020-03-07 13:05:07 -0300

    Two implementations, add another test.

--- src/shared.rs
+++ src/shared.rs
@@ -3,11 +3,25 @@ use std::cell::RefCell;
 use std::iter::IntoIterator;
 use std::rc::Rc;
 
-pub struct Shared<T> where T: Clone {
+pub struct Shared<T> {
 	data: Rc<RefCell<T>>,
 }
 
+#[cfg(not(feature = "iter"))]
+//#[cfg(feature = "iter")]
 impl<'a, K, T: IntoIterator<Item=K>> IntoIterator for &'a Shared<T>
+	where T: IntoIterator<Item=K> + Clone {
+	type Item = K;
+	type IntoIter = T::IntoIter;
+
+	fn into_iter(self) -> Self::IntoIter {
+		let self_ref: &RefCell<T> = self.data.borrow();
+		self_ref.clone().into_inner().into_iter()
+	}
+}
+
+#[cfg(feature = "iter")]
+impl<'a, K, T: Iterator<Item=K>> IntoIterator for &'a Shared<T>
 	where T: Iterator<Item=K> + Clone {
 	type Item = K;
 	type IntoIter = T;
@@ -18,16 +32,13 @@ impl<'a, K, T: IntoIterator<Item=K>> IntoIterator for &'a Shared<T>
 	}
 }
 
-
 #[cfg(test)]
 mod tests {
-	use crate::{
-		object::Object,
-		shared::Shared,
-	};
+	use crate::shared::Shared;
 	use std::iter::IntoIterator;
 	use std::rc::Rc;
 	use std::cell::RefCell;
+	use std::borrow::Borrow;
 
 	#[derive(Clone, Debug, Default, PartialEq, Eq)]
 	struct Test {
@@ -54,13 +65,17 @@ mod tests {
 			data: Rc::new(RefCell::new(test))
 		};
 
-		{
-			let mut x = 1;
-			for it in &my_shared {
-				assert_eq!(it, x);
-				x += 1;
-			}
-			assert_eq!(x, 4);
+		let mut x = 1;
+		for it in &my_shared {
+			assert_eq!(it, x);
+			x += 1;
 		}
+		assert_eq!(x, 4);
+
+		let mut my_it = my_shared.borrow().into_iter();
+		assert_eq!(my_it.next().unwrap(), 1);
+		assert_eq!(my_it.next().unwrap(), 2);
+		assert_eq!(my_it.next().unwrap(), 3);
+		assert!(my_it.next().is_none());
 	}
 }
